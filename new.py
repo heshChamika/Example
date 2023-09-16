08d3ea438f1cb3fe4e6df3214cdfdd802089d5a0
heshChamika <127346128+heshChamika@users.noreply.github.com>
2023-09-16 22:06:57 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,12 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+x = np.arange(0,math.pi*2,0.05)
+
+y = np.sin(x)
+plt.plot(x,y)
+plt.xlabel("Angel")
+plt.ylabel("sine")
+plt.title('sine wave')
+plt.show()
